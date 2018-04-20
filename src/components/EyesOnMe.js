a1a1060415ce3eec8f061e1b96e30e904559a022
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-20 20:10:29 +0000

    automatically backed up by learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -2,13 +2,9 @@
 import React from 'react'
 
 class EyesOnMe extends React.Component {
-  constructor() {
-    super(props)
-
     function keyUp () {
       console.log('Entering password...')
     }
-  }
 
   render () {
     return (
