736c8eef04040dc387a1356f9b9efe4a869424ac
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-20 20:10:23 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,13 +2,10 @@
 import React from 'react'
 
 class Keypad extends React.Component {
-  constructor() {
-    super(props)
 
     function keyUp () {
       console.log('Entering password...')
     }
-  }
 
   render () {
     return (
